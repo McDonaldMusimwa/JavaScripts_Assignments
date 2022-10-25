627b0880900bdb8d7e1fed06148a0be005a78804
McDonald <mus22003@byui.edu>
2022-10-25 14:17:10 +0200

    changed the component to meme search

--- week06/data.js
+++ week06/data.js
@@ -1,27 +1,69 @@
-const url='https://geek-jokes.sameerkumar.website/api?format=json';
+const url = 'https://api.imgflip.com/get_memes';
 
-async function getcardata(url){
+async function getdata(url) {
     const response = await fetch(url);
 
-    if (response.ok){
+    if (response.ok) {
 
-        const data = await response.json();
-        
-        dothis(data)
+        const Data = await response.json();
+
+
+        dothis(Data)
     }
 }
 
-function dothis(info){
 
-const element = document.querySelector("#display")
-element.innerHTML=info.joke
+function dothis(item) {
+    let array = item.data.memes;
+    console.log(array)
+    array.forEach(item => {
+        const element = document.querySelector(".display")
+
+        let name = document.createElement("label")
+        name.innerHTML = item.name
+        let picture = document.createElement("picture")
+
+        let meme = document.createElement("img")
+        meme.setAttribute("src", item.url)
+        picture.appendChild(name)
+        picture.appendChild(meme)
+
+
+
+
+        element.append(picture)
+
+
+    });
 
 }
 
-getcardata(url);
+function search() {
+    var input, filter, display, picture, label, i, txtValue;
+    input=document.getElementById("myInput")
+    filter = input.value.toUpperCase();
+    display = document.querySelector(".display");
+    picture = display.getElementsByTagName("picture");
 
-function getnewjoke(){
+    for (i = 0; i < picture.length; i++) {
+        label = picture[i].getElementsByTagName("label")[0];
+        console.log(label)
+        txtValue = label.textContent || label.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            picture[i].style.display = "";
+        } else {
+            picture[i].style.display = "none";
+        }
+
+    }
+
+}
+
+
+getdata(url);
+
+function getnewjoke() {
     window.location.reload(true);
 }
 
-document.querySelector("#refresh").addEventListener("click",getnewjoke)
+document.querySelector("#myInput").addEventListener("keyup", search)
